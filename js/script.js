27975db83f3e636401ed50e0b8aaa98f689a416a
cinvenz <vincenzobortune@gmail.com>
2022-11-23 10:12:38 +0100

    add v-cloak

--- js/script.js
+++ js/script.js
@@ -4,19 +4,12 @@ new Vue({
 		arrEmail: [],
 	},
 	methods: {
-        // getEmail() {
-		// 	axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-		// 		.then(axiosResponse => {
-		// 			// console.log(axiosResponse);
-		// 			this.arrEmail.push(axiosResponse.data.response);
-		// 		})
-		// },
 		getEmail() {
 			this.arrEmail = [];
 			for (let i = 0; i < 10; i++) {
 				axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
 				.then(axiosResponse => {
-					// console.log(axiosResponse);
+					console.log(axiosResponse);
 					this.arrEmail.push(axiosResponse.data.response);
 				})
 			}
